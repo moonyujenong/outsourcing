8f8ec586fe04691030479bd540801110a022d42b
moonyujenong <sogml308@gmail.com>
2023-05-10 14:52:12 +0900

    수정 사항 반영

--- work/Cosmax/assets/js/index.js
+++ work/Cosmax/assets/js/index.js
@@ -1,4 +1,7 @@
 $(function () {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
   /* sound controls */
   $(".sound").click(function () {
     var control = $(this).children("img");
@@ -15,12 +18,12 @@ $(function () {
   });
 
   // scroll 감지 (모바일 전용)
-  $(".theme-main").scroll(function () {
+  $(".theme-main").click(function () {
     $(".drag_img").addClass("none");
   });
 
   //비디오 자동 재생
-  $(".bg_video").play();
+  $(".bg_video").trigger("play");
 
   // 비디오 끝났음 감지
   $(".bg_video").on("ended", function () {
